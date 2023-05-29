f0a3e0adea07f2322b4091fbb529b0f971284619
Rafif Idul Fitra <96464544+rapit@users.noreply.github.com>
2023-05-29 22:38:14 +0700

    Add files via upload

---
+++ src/Components/Navbar/Menu.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+export const Menu = [
+  {
+    title : "Home",
+    url : "/",
+    cName : "nav-links",
+    icon : "fa-solid fa-house"
+  },
+  {
+    title : "About",
+    url : "About",
+    cName : "nav-links",
+    icon : "fa-solid fa-circle-info"
+  },
+  {
+    title : "AddBook",
+    url : "#",
+    cName : "nav-links",
+    icon : "fa-solid fa-book"
+  },
+  {
+    title : "Profile",
+    url : "Profile",
+    cName : "nav-links",
+    icon : "fa-solid fa-user",
+  },
+  {
+    title : "Login",
+    url : "Login",
+    cName : "nav-links-mobile",
+  }
+]
